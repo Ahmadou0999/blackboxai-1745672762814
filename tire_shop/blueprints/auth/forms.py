f1a53d44a27c4052b586200d7aa4f2cf824978f5
Ahmadou0999 <133282263+Ahmadou0999@users.noreply.github.com>
2025-04-26 15:06:30 +0200

    Add tire_shop/blueprints/auth/forms.py 1745672790075

---
+++ tire_shop/blueprints/auth/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
+from wtforms.validators import DataRequired, Email, EqualTo
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    is_admin = BooleanField('Admin')
+    submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
